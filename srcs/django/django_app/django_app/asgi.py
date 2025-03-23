542ded973b765e7e2a0b3eae36e8cd1d5c2e25de
Max <max@manbex.fr>
2025-03-23 15:07:01 +0100

    Custom auth middleware added for websocket connections

--- srcs/django/django_app/django_app/asgi.py
+++ srcs/django/django_app/django_app/asgi.py
@@ -8,19 +8,22 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django_app.middleware import TokenAuthMiddlewareStack
 import chat.routing
 import pong.routing
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": TokenAuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns + pong.routing.websocket_urlpatterns
         )
