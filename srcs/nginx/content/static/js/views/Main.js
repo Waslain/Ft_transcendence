4645d53934a8c134b438f8b6c283b9dd4f5e5778
CC <tanjo@umail.iu.edu>
2025-03-29 10:36:05 +0100

    change main view style

--- srcs/nginx/content/static/js/views/Main.js
+++ srcs/nginx/content/static/js/views/Main.js
@@ -13,6 +13,9 @@ export default class extends AbstractView {
       backdrop-filter: blur(5px);
 			border-radius: 1rem;
 		}
+    .welcome-text {
+      font-size: 30px;
+    }
     `;
   }
 
@@ -21,21 +24,61 @@ export default class extends AbstractView {
     <div class="container py-5 h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col col-xl-10 mt-5">
-          <div class="card container-wrapper border-light ">
-            <div class="row g-0">
-              <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                <div class="card-body p-4 p-lg-5 text-black">
-  
-                  <h1 class="text-white">I'm a sample page</h1>
-			            <a href="/pong" class="nav__link" data-link>pong</a><br>
-
-                </div>
-              </div>
+          <div class="card container-wrapper border-light">
+            <div class="card-body p-4 p-lg-5 d-flex justify-content-center align-items-center">
+              <div class="welcome-text"></div>
             </div>
+          <div class="card-footer border-light text-center">
+	              <a href="/pong" class="nav__link" style="font-size: 20px;" data-link>Start Pong</a>
+          </div>
           </div>
         </div>
       </div>
     </div>
 		`;
+  }
+
+  async getJavaScript() {
+    function scrambleText(){
+      const phrases = ['Hello!', 'Welcome to', 'Transendence', 'Have fun C:'];
+      const specialChars = '!@#$%^&*()+_[]{}~<>?|/__\\';
+      const el = document.querySelector('.welcome-text');
+  
+      let currentIndex = 0;
+
+      el.textContent = phrases[currentIndex];
+      el.style.color = 'white';
+      setInterval(() => {
+        currentIndex = (currentIndex + 1) % phrases.length;
+        const nextPhrase = phrases[currentIndex];
+
+        textAnimation(el, nextPhrase);
+      },3000);
+
+      function textAnimation(el, nextText) {
+        let iter = 0;
+        const maxiter = 5;
+      
+        // Timer for the scramble animation
+        const interval = setInterval(() => {
+          if (iter < maxiter) {
+            let scrambledText = '';
+            for (let i = 0; i < nextText.length; i++) {
+              const randomIndex = Math.floor(Math.random() * specialChars.length);
+              scrambledText += specialChars[randomIndex];
+            }
+            
+            el.textContent = scrambledText;
+            el.style.color = '#c0c2c9';
+            iter++;
+          } else {
+            clearInterval(interval);
+            el.textContent = nextText;
+            el.style.color = 'white';
+          }
+        }, 125);
+      }
+    }
+    scrambleText();
   }
 }
